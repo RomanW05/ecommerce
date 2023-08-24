dfdd0bba248a752bd125629100091f60f0b82b06
Roman <romanwn@mail.com>
2023-08-24 14:14:09 +0300

    creating tables at startup not working

--- ecommerce/backend_analytics/create_db.py
+++ ecommerce/backend_analytics/create_db.py
@@ -9,12 +9,13 @@ from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 import analytics_models
 import os
-
+import sys
+print(os.environ)
 # Option 1, using manual transaction
 # engine = create_engine(f"postgresql://{username}:{password}@{host}/{database_name}")
 
 # engine = create_engine(analytics_db_url)  # Using f"postgresql://{username}:{password}@{host}"
-engine = create_engine(f"postgres+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}")
+engine = create_engine(f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}")
 # Session = sessionmaker(bind=engine)
 conn = engine.connect()
 # tables = [
--- ecommerce/backend_analytics/kafka_consume.py
+++ ecommerce/backend_analytics/kafka_consume.py
@@ -10,7 +10,7 @@ from .analytics_models import RawRequest
 
 
 
-engine = create_engine(f"postgres+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}")
+engine = create_engine(f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}")
 Session = sessionmaker(bind=engine)
 
 
