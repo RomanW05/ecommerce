547e333442dc77a7e9c68b9b63a45e4eed9d95d8
Roman <romanwn@mail.com>
2024-01-11 23:44:51 +0200

    Created Users model

--- microservice_users/users/models.py
+++ microservice_users/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    id = models.CharField(primary_key=True, editable=False)
+    username = models.CharField('Username', unique=True)
+    email = models.EmailField('Email address', unique=True)
+    date_joined = models.DateTimeField('Date joined', auto_now_add=True)
+    blocked_status = models.BooleanField('Blocked status', default=False)
+    name = models.CharField('Name', max_length=60)
+    surname = models.CharField('Surname', max_length=60)
+    phone_number = models.CharField('Phone number', max_length=20, unique=True)
+
+
+
+
+    
+    
