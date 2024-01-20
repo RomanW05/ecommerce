ad1c5426caa137ad5104e8429043bcf6bb7d5db6
Roman <romanwn@mail.com>
2024-01-20 15:28:33 +0200

    Added manager for user creation

--- microservice_users/users/models.py
+++ microservice_users/users/models.py
@@ -1,40 +1,9 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import gettext_lazy as _
 
+from .manager import MyUserManager
 # Create your models here.
-
-
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, date_joined, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(
-            email=self.normalize_email(email),
-            date_joined=date_joined,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
     
 
 class User(AbstractBaseUser):
