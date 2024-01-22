51fc90c7b78fad78fa594faf0e28cd987d8f3718
Roman <romanwn@mail.com>
2024-01-22 18:42:34 +0200

    Changed SECRET_KEY to environment variable

--- microservice_users/microservice_users/settings.py
+++ microservice_users/microservice_users/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yvr&oa%l2&8!r&bjfh+@@x5cyg7r5qs^b^0do$gedj6vf1%yuc'
+SECRET_KEY = os.getenv ('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
