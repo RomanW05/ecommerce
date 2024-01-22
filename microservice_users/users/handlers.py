c4605c8b9e6cec4507e6f648bf5faeec3457fb90
Roman <romanwn@mail.com>
2024-01-22 18:47:04 +0200

    Added cache blacklist token

--- microservice_users/users/handlers.py
+++ microservice_users/users/handlers.py
@@ -1,4 +1,13 @@
 from django.core.mail import send_mail
+from django.contrib.auth.base_user import BaseUserManager
+from datetime import datetime
+from django.core.cache import cache
+from rest_framework_simplejwt.tokens import UntypedToken
+import logging
+from django.conf import settings
+
+logger = logging.getLogger('main')
+
 
 def send_registration_email(user_email):
     send_mail(
@@ -8,3 +17,57 @@ def send_registration_email(user_email):
         [user_email],
         fail_silently=False,
     )
+
+
+class UserManager(BaseUserManager):
+    
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.date_joined = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+def cache_blacklisted_token(token):
+    try:
+        untyped_token = UntypedToken(token)
+        jti = untyped_token['jti']
+        cache.set(jti, 'blacklisted', timeout=settings.EXPIRE_TIME_REFRESH_IN_SECS)  # Cache for 1 hour
+    except Exception as e:
+        logger.info(f'Error in caching token: {e}')
+
+def is_token_blacklisted(token):
+    try:
+        untyped_token = UntypedToken(token)
+        jti = untyped_token['jti']
+        if cache.get(jti) == 'blacklisted':
+            return True
+        else:
+            return False
+    except Exception as e:
+        logger.info(f"Error checking token: {e}")
+        return True
