6972bd369086dcb57f98d09fdbb16b2e2082f507
Roman <romanwn@mail.com>
2024-01-22 18:50:31 +0200

    Added logout and test_route to urls

--- microservice_users/users/urls.py
+++ microservice_users/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import (RegisterUser, LoginUser)
+from .views import (RegisterUser, LoginUser, TestToken, BlacklistRefreshView)
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='Register new user'),
     path('login/', LoginUser.as_view(), name='Login'),
+    path('test_token/', TestToken.as_view(), name='Test token'),
+    path('logout/', BlacklistRefreshView.as_view(), name="logout"),
 ]
