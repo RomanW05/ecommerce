4e9214f3a21f516913ae71f03fa7f0434924abdb
Roman <romanwn@mail.com>
2024-01-23 16:59:45 +0200

    Added jti to token payload

--- microservice_users/users/authentication.py
+++ microservice_users/users/authentication.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
 import jwt
+import uuid
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed, ParseError
+
+from .handlers import is_token_blacklisted
 import logging
 
 
@@ -13,10 +16,9 @@ User = get_user_model()
 
 class JWTAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        # Extract the JWT from the Authorization header
         jwt_token = request.META.get('HTTP_AUTHORIZATION')
         if jwt_token is None:
-            return None
+            raise AuthenticationFailed('No token provided')
 
         jwt_token = self.get_the_token_from_header(jwt_token)  # clean the token
 
@@ -32,28 +34,42 @@ class JWTAuthentication(BaseAuthentication):
         user_id = payload.get('user_id')
         if user_id is None:
             raise AuthenticationFailed('User identifier not found in JWT')
+        
+        if is_token_blacklisted(jwt_token):
+            raise AuthenticationFailed('Token expired')
+        
+        return payload, None
 
-        user = User.objects.filter(id=user_id).first()
-        if user is None:
-            raise AuthenticationFailed('User not found')
-
-        # Return the user and token payload
-        return user, payload
-
+ 
     def authenticate_header(self, request):
         return 'Bearer'
 
     @classmethod
-    def create_jwt(cls, user):
+    def create_refresh_jwt(cls, user):
         # Create the JWT payload
         payload = {
             'user_id': user.pk,
-            'exp': int((datetime.now() + timedelta(seconds=settings.REFRESH_TOKEN_LIFETIME)).timestamp()),
-            'iat': datetime.now().timestamp()
+            'exp': datetime.utcnow() + timedelta(seconds=settings.REFRESH_TOKEN_LIFETIME),
+            'iat': datetime.utcnow(),
+            'jti': str(uuid.uuid4())
         }
         # Encode the JWT with your secret key
-        jwt_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-        return jwt_token
+        refresh_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+        return refresh_token
+    
+    @classmethod
+    def create_access_jwt(cls, user):
+        # Create the JWT payload
+        payload = {
+            'user_id': user.pk,
+            'exp': (datetime.utcnow() + timedelta(seconds=settings.REFRESH_TOKEN_LIFETIME)),
+            'iat': datetime.utcnow(),
+            'jti': str(uuid.uuid4())
+        }
+        # Encode the JWT with your secret key
+        access_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+        return access_token
+
 
     @classmethod
     def get_the_token_from_header(cls, token):
