262c885a5cb2b82ee26f3907baea6ada2c62f59b
Roman <romanwn@mail.com>
2024-01-23 12:06:35 +0200

    Fixed auto add date_joined issue

--- microservice_users/users/manager.py
+++ microservice_users/users/manager.py
@@ -1,25 +1,23 @@
 from django.contrib.auth.base_user import BaseUserManager
 
+
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, date_joined, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Creates and saves a User with the given email and password.
         """
         if not email:
             raise ValueError("Users must have an email address")
         email = self.normalize_email(email)
-        username = email.split('@')[0]
         user = self.model(
             email=email,
-            date_joined=date_joined,
-            username=username
+            **extra_fields
         )
-
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, password=None, **extra_fields):
         """
         Creates and saves a superuser with the given email and password.
         """
