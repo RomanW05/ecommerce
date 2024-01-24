a42b1d03afccf9bbcdaf829415fb254c43217107
Roman <romanwn@mail.com>
2024-01-24 13:27:52 +0200

    Removed profile route

--- microservice_users/users/urls.py
+++ microservice_users/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import (RegisterUser, LoginUser, TestToken, BlacklistRefreshView)
+from .views import (RegisterUser, LoginUser, TestToken, LogoutUser, AlwaysOK)
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='Register new user'),
     path('login/', LoginUser.as_view(), name='Login'),
     path('test_token/', TestToken.as_view(), name='Test token'),
-    path('logout/', BlacklistRefreshView.as_view(), name="logout"),
+    path('logout/', LogoutUser.as_view(), name="Logout"),
+    path('alwaysok/', AlwaysOK.as_view(), name="AlwaysOK"),
+
 ]
