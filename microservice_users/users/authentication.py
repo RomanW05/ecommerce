851549d4b31c91436855f7e50db2b829a2706fdd
Roman <romanwn@mail.com>
2024-01-24 20:45:32 +0200

    Added expire date check to token

--- microservice_users/users/authentication.py
+++ microservice_users/users/authentication.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 import jwt
+from jwt.exceptions import ExpiredSignatureError
 import uuid
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed, ParseError
+from rest_framework import exceptions
 
 from .handlers import is_token_blacklisted
 import logging
@@ -22,14 +24,19 @@ class JWTAuthentication(BaseAuthentication):
 
         jwt_token = self.get_the_token_from_header(jwt_token)  # clean the token
 
-        # Decode the JWT and verify its signature
         try:
             payload = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=['HS256'])
-        except jwt.exceptions.InvalidSignatureError:
+            exp = payload.get('exp')
+            if datetime.utcnow() > datetime.utcfromtimestamp(exp):
+                raise ExpiredSignatureError("Token has expired")
+        except jwt.InvalidSignatureError:
             raise AuthenticationFailed('Invalid signature')
-        except:
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed('Invalid signature')
+        except Exception as e:
+            logger.debug(e)
             raise ParseError()
-
+        
         # Get the user from the database
         user_id = payload.get('user_id')
         if user_id is None:
@@ -37,7 +44,7 @@ class JWTAuthentication(BaseAuthentication):
         
         if is_token_blacklisted(jwt_token):
             raise AuthenticationFailed('Token expired')
-        
+
         return payload, None
  
     def authenticate_header(self, request):
@@ -86,4 +93,17 @@ class JWTAuthentication(BaseAuthentication):
     def get_the_token_from_header(cls, token):
         token = token.replace('Bearer', '').replace(' ', '')  # clean the token
         return token
+    
+    @classmethod
+    def is_token_valid(cls, token):
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        except jwt.InvalidSignatureError:
+            return False
+        compare = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+        if token == compare:
+            return True
+        else:
+            return False
+
 
