7ed069cf37388b7f1d0765ffbca9b2c3c538b869
Roman <romanwn@mail.com>
2024-01-24 20:43:11 +0200

    Added forgot password

--- microservice_users/users/views.py
+++ microservice_users/users/views.py
@@ -7,8 +7,9 @@ from rest_framework_simplejwt.exceptions import TokenError
 
 
 from .authentication import JWTAuthentication
-from .handlers import send_registration_email, cache_blacklisted_token, is_token_blacklisted
-from .serializer import (LoginSerializer, ResisterSerializer)
+from .forms import PasswordResetForm
+from .handlers import send_registration_email, cache_blacklisted_token, is_token_blacklisted, send_custom_email
+from .serializer import (LoginSerializer, ResisterSerializer, ForgotPasswordSerializer)
 
 
 User = get_user_model()
@@ -64,6 +65,7 @@ class LoginUser(generics.GenericAPIView):
 class TestToken(generics.GenericAPIView):
 
     def post(self, request):
+        logger.debug(request.data)
         refresh = request.data.get('refresh')
         if is_token_blacklisted(refresh):
             return Response({"message":"Session expired"}, status=status.HTTP_401_UNAUTHORIZED)
@@ -95,3 +97,47 @@ class AlwaysOK(generics.GenericAPIView):
     def get(self, request):
         return Response({"message": "ok"}, status=status.HTTP_200_OK)
 
+
+class ForgotPassword(generics.GenericAPIView):
+    authentication_classes = []
+    serializer_class = ForgotPasswordSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            user = User.objects.filter(email=email).first()
+            
+            if user is None:
+                return Response({"message": "User not found"}, status=status.HTTP_400_NOT_FOUND)
+            reset_token = JWTAuthentication.create_password_reset_jwt(user)
+            send_custom_email(user, 'forgotten_password', reset_token=reset_token)
+
+            return Response({"message": "Reset password sent to email"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Not valid"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPassword(generics.GenericAPIView):
+    
+    def post(self, request):
+        form = PasswordResetForm(request.data)
+        if form.is_valid():
+            user_id = request.user.get('user_id') if hasattr(request, 'user') and request.user else None
+            if user_id is None:
+                return Response({"message":"Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+            user = User.objects.get(id=user_id)
+            if user is None:
+                return Response({"message":"No user found"}, status=status.HTTP_400_BAD_REQUEST)
+
+            jwt_token = request.META.get('HTTP_AUTHORIZATION')
+            if not JWTAuthentication.is_token_valid(jwt_token.split()[1]):
+                return Response({"message":"Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            new_password = form.cleaned_data['new_password']
+            user.set_password(new_password)
+            user.save()
+            return Response({"message": 'Passwords changed'}, status=status.HTTP_202_ACCEPTED)
+        else:
+            logger.debug(form.errors)
+            return Response({"message": 'Invalid password fields'}, status=status.HTTP_400_BAD_REQUEST)
