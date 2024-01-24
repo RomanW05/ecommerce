c90b86dab681c085eafe3787f53f2202352274f1
Roman <romanwn@mail.com>
2024-01-24 13:24:11 +0200

    Moved models fields

--- microservice_users/users/admin.py
+++ microservice_users/users/admin.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
 
 # from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import Country, Province, ZipCode, Address
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -83,7 +82,3 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Country)
-admin.site.register(Province)
-admin.site.register(ZipCode)
-admin.site.register(Address)
