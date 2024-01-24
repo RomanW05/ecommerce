589a1078bfbe5faee70f0e6ee404f7f564a5eeb1
Roman <romanwn@mail.com>
2024-01-24 20:46:03 +0200

    Added password reset options

--- microservice_users/users/urls.py
+++ microservice_users/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import (RegisterUser, LoginUser, TestToken, LogoutUser, AlwaysOK)
+from .views import (RegisterUser, LoginUser, TestToken, LogoutUser, AlwaysOK, ForgotPassword, ResetPassword)
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='Register new user'),
@@ -8,5 +8,9 @@ urlpatterns = [
     path('test_token/', TestToken.as_view(), name='Test token'),
     path('logout/', LogoutUser.as_view(), name="Logout"),
     path('alwaysok/', AlwaysOK.as_view(), name="AlwaysOK"),
+
+    path('forgot_password/', ForgotPassword.as_view(), name='Forgot password'),
+    path('reset_password/', ResetPassword.as_view(), name='Reset password')
+
 
 ]
