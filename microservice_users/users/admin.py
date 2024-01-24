0e4afb7f4b0d7795f16a57f077060f7b0612cf3c
Roman <romanwn@mail.com>
2024-01-24 13:43:15 +0200

    Changed is_admin to is_superuser

--- microservice_users/users/admin.py
+++ microservice_users/users/admin.py
@@ -59,11 +59,11 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ["email", "is_admin"]
-    list_filter = ["is_admin"]
+    list_display = ["email", "is_superuser"]
+    list_filter = ["is_superuser"]
     fieldsets = [
         (None, {"fields": ["email", "password"]}),
-        ("Permissions", {"fields": ["is_admin"]}),
+        ("Permissions", {"fields": ["is_superuser"]}),
     ]
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
