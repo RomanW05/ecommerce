ec3bd3a9942054dc89cae6673eeb20a988172295
Roman <romanwn@mail.com>
2024-01-29 12:15:58 +0200

    Added product info url

---
+++ microservice_inventory/inventory/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import ProductInfo
+
+urlpatterns = [
+    path('product/<int:product_id>', ProductInfo.as_view(), name='Single product information'),
+]   
