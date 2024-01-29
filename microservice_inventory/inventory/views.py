1d52f7ad23c94961f9caec661ed40fab4467a5ad
Roman <romanwn@mail.com>
2024-01-29 12:15:18 +0200

    Added product info view

--- microservice_inventory/inventory/views.py
+++ microservice_inventory/inventory/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .models import Products
 
 # Create your views here.
+
+
+class ProductInfo(generics.GenericAPIView):
+    def get(self, request):
+        product_id = request.GET.get("product_id")
+        product_info = Products.objects.filter(id=product_id).first()
+        if product_info is None:
+            return Response({'message': "Product not found"}, status=status.HTTP_400_NOT_FOUND)
+        return Response({'message': "ok", "data":product_info}, status=status.HTTP_200_OK)
+
+
