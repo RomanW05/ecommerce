e5fb31211373e557e0addc3e8fe75c9add50aaf0
Roman <romanwn@mail.com>
2024-02-01 12:28:34 +0200

    Changed STATIC_URL to match url routing and nginx

--- microservice_inventory/microservice_inventory/settings.py
+++ microservice_inventory/microservice_inventory/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = f'{BASE_DIR}/'
+STATIC_URL = f'/inventory/static/'
 STATIC_ROOT = f'{BASE_DIR}/static/'
 
 # Default primary key field type
