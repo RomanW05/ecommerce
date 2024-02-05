62d43a75fbca7dd48b3450d53b32ec26f043b049
Roman <romanwn@mail.com>
2024-02-05 21:18:12 +0200

    Added production or development server handling

--- microservice_inventory/entrypoint.sh
+++ microservice_inventory/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn microservice_inventory.wsgi:application --bind 0.0.0.0:8003
+
+if [ "$DJANGO_ENV" = "development" ]; then
+    echo "Starting Django development server"
+    python manage.py runserver 0.0.0.0:8003
+else
+    echo "Starting Gunicorn server"
+    gunicorn microservice_inventory.wsgi:application --bind 0.0.0.0:8003
+fi
