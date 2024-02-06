f0bacde102f1252001240f7fa4f5a93321f168cc
Roman <romanshape@gmail.com>
2024-02-06 22:53:31 +0200

    Added Latest Arrivals url

--- microservice_inventory/inventory/urls.py
+++ microservice_inventory/inventory/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductInfo
+from .views import ProductInfo, LatestArrivals
 
 urlpatterns = [
     path('product/<int:product_id>/', ProductInfo.as_view(), name='Single product information'),
+    path('latest_arrivals/', LatestArrivals.as_view(), name='Latest arrivals'),
 ]   
